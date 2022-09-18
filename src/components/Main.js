25573e1e364e7280eb9134ec03f8a120fc61d01b
tomplucinski <tapluci1@gmail.com>
2022-09-18 13:03:00 -0700

    Add alt tags for pics

--- src/components/Main.js
+++ src/components/Main.js
@@ -45,7 +45,7 @@ class Main extends React.Component {
             View Code
           </a>
           <span className="image main">
-            <img src={algoPic} alt="" />
+            <img src={algoPic} alt="algorithms" />
           </span>
           <h3>PluggedIn</h3>
           <p>
@@ -72,7 +72,7 @@ class Main extends React.Component {
             View Code
           </a>
           <span className="image main">
-            <img src={pic01} alt="" />
+            <img src={pic01} alt="pluggedIn" />
           </span>
           <h3>Cheer</h3>
           <p>
@@ -93,7 +93,7 @@ class Main extends React.Component {
             View Code
           </a>
           <span className="image main">
-            <img src={pic04} alt="" />
+            <img src={pic04} alt="cheer" />
           </span>
 
           {close}
